f37a6293653c9dc50c88cca62b31248ffeca547d
Leon Trestman <44327319+LeonTrestman@users.noreply.github.com>
2022-01-15 20:01:26 +0200

    Add files via upload

---
+++ 20554 Advanced Programming with Java/Exams/2021b-78/Q2/SampleLauncher.java
@@ -0,0 +1,27 @@
+/*
+Maman 13 Q2, Leon Trestman , 311135719, simple calculator application
+*/
+
+/*Represents launcher for simple calculator*/
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class SampleLauncher extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Sample.fxml"));
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setTitle("Name");
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
